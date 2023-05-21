d19e4870c03fc97f7ba344fe3f32ce87b47320d0
17计科-刘沅沅 <1580487107@qq.com>
2023-05-21 19:55:48 +0800

    添加404页面

---
+++ src/views/Error404.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="error">
+    <div class="info">404</div>
+    <div>
+        <router-link to="/" class="a-link">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.error{
+    text-align: center;
+    padding-top: 200px;
+    .info{
+        font-size: 100px;
+        color: rgb(52, 187, 241);
+    }
+}
+</style>
